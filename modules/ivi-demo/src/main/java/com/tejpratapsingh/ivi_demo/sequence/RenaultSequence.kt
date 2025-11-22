323791c6afd235cb25e6b044e4ce13092c134e9e
tejpratapsingh <tej.singh@catalyzesystems.com>
2025-11-22 12:41:39 +0530

    chore: migrate to modules

---
+++ modules/ivi-demo/src/main/java/com/tejpratapsingh/ivi_demo/sequence/RenaultSequence.kt
@@ -0,0 +1,35 @@
+package com.tejpratapsingh.ivi_demo.sequence
+
+import RenaultCar
+import android.content.Context
+import com.tejpratapsingh.motionlib.core.MotionConfig
+import com.tejpratapsingh.motionlib.core.VideoAspectRatio
+import com.tejpratapsingh.motionlib.core.motion.BaseContourMotionView
+import com.tejpratapsingh.motionlib.core.motion.MotionVideoProducer
+
+fun sampleMotionVideo(applicationContext: Context): MotionVideoProducer {
+    val motionConfig = MotionConfig(
+        aspectRatio = VideoAspectRatio.Ratio9x16_480, fps = 30
+    )
+
+    val assetManager = applicationContext.assets
+    val files = assetManager.list(RenaultCar.imageAssetSubFolder)
+
+    val motionView: BaseContourMotionView = RenaultCar(
+        context = applicationContext,
+        startFrame = 1,
+        endFrame = files?.size ?: 1
+    )
+
+    val motionView2: BaseContourMotionView = RenaultCar(
+        context = applicationContext,
+        startFrame = 1,
+        endFrame = 55000
+    )
+
+    return MotionVideoProducer.with(
+        context = applicationContext,
+        config = motionConfig,
+    ).addMotionViewToSequence(motionView = motionView)
+        .addMotionViewToSequence(motionView = motionView2)
+}
