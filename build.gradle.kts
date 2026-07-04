a50aa8b5304f173dc053d59c584d75f839703f9f
Tej Pratap Singh <tejpratap46@gmail.com>
2026-07-04 21:53:31 +0530

    ci: refactor detekt workflow to use Gradle task (#79)
    * ci: update workflow to trigger on pull requests to the `master` branch
    * ci: replace `natiginfo/action-detekt-all` with `./gradlew detekt` execution
    * ci: add JDK 17 and Gradle setup steps to the workflow environment
    
    Co-authored-by: tejpratapsingh <tej.singh@catalyzesystems.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,11 +8,38 @@ buildscript {
     }
     dependencies {
         classpath(libs.ktlint.gradle)
+        classpath(libs.detekt.gradle)
     }
 }
 
 plugins {
     alias(libs.plugins.ktlint)
+    alias(libs.plugins.detekt)
+}
+
+detekt {
+    toolVersion = libs.versions.detekt.get()
+    config.setFrom(file("config/detekt/detekt.yml"))
+    buildUponDefaultConfig = true
+    allRules = false
+}
+
+subprojects {
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    detekt {
+        config.setFrom(rootProject.file("config/detekt/detekt.yml"))
+    }
+}
+
+tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+    reports {
+        html.required.set(true)
+        xml.required.set(true)
+        txt.required.set(false)
+        sarif.required.set(true)
+        md.required.set(false)
+    }
 }
 
 ktlint {
